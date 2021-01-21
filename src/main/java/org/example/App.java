561551e85b1fab80ee19230b0ddfea4fa281a5fd
Lukas Baum <lbaum@conet.de>
2021-01-21 09:38:06 +0100

    add login page

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -17,7 +17,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("login"), 800, 500);
+        stage.setMinHeight(500);
+        stage.setMinWidth(800);
         stage.setScene(scene);
         stage.show();
     }
