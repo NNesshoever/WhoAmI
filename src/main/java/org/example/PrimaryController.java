58c98c29c352e8836741c5c809403f6f1c50e5bc
Max Döge <mdoege@conet.de>
2021-01-21 09:16:38 +0100

    Revert "new folder structure, added client project"
    This reverts commit 21b0a03ae13de17bf81103d6ef128d16add1536c.

--- src/main/java/org/example/PrimaryController.java
+++ src/main/java/org/example/PrimaryController.java
@@ -6,7 +6,7 @@ import javafx.fxml.FXML;
 public class PrimaryController {
 
     @FXML
-    private void login() throws IOException {
-        App.setRoot("menu");
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
     }
 }
