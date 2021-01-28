2474c1e90982aa2cdf3ab09beac830163886645e
Niclas Neßhöver <NNesshoever@conet.de>
2021-01-28 08:21:48 +0100

    Addes Methods in Controller

--- src/main/java/org/example/StartseiteController.java
+++ src/main/java/org/example/StartseiteController.java
@@ -1,6 +1,23 @@
 package org.example;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class StartseiteController {
 
-    
+    @FXML
+    Button RefreshButton;
+    @FXML
+    Button LogoutButton;
+
+    @FXML
+    public void RefreshPlayers(){
+
+    }
+    @FXML
+    public void LogoutUser(){
+
+    }
+
+
 }
